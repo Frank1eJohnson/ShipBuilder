fcf1695fe206e1995269f9c59927893d94d02d07
Zhongqi Shan <60977360+Sabrave@users.noreply.github.com>
2020-07-19 14:37:45 +0800

    Fix text visibility regression in UE5

--- Source/ShipBuilder/ShipBuilder.build.cs
+++ Source/ShipBuilder/ShipBuilder.build.cs
@@ -43,7 +43,7 @@ public class ShipBuilder : ModuleRules
         if (Target.Platform.IsInGroup(UnrealPlatformGroup.Windows))
         {
             PrivateDependencyModuleNames.AddRange(new string[] {
-                "DLSS"
+                //"DLSS"
             });
         }
 
